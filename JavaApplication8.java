630b45b9e1966ebe6abc3bbc6b4b648d1048aadc
Naidr <118465343+Naidr@users.noreply.github.com>
2022-11-27 20:55:03 +0000

    Add files via upload

---
+++ JavaApplication8.java
@@ -0,0 +1,8 @@
+package javaapplication8;
+public class JavaApplication8 {
+    public static void main(String[] args) {
+        String cadena1 = "Hola";
+        String cadena2 = null;
+        System.out.println(cadena1.equals(cadena2));        
+    }    
+}

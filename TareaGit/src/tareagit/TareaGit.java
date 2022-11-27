f9a8fbcfdf3d2d1b3d32031adb13a23a2174fd90
Naidr <118465343+Naidr@users.noreply.github.com>
2022-11-27 21:24:38 +0000

    Add files via upload

---
+++ TareaGit/src/tareagit/TareaGit.java
@@ -0,0 +1,14 @@
+package tareagit;
+
+public class TareaGit {
+
+    public static void main(String[] args) {
+        int n=5;
+        int result=0;
+        for(int i=0;i<=n;i++){
+            result+=i+i;
+        }
+        System.out.println(result);
+    }
+    
+}
